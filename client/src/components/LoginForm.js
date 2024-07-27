72d0ba6f29159bc7fa9b764603e1a58bd21a08b5
Kevin Choi <kevinnwjchoi@gmail.com>
2024-07-27 16:23:13 -0400

    option to sign up in login page

--- client/src/components/LoginForm.js
+++ client/src/components/LoginForm.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import { Formik, Form, Field, ErrorMessage } from 'formik';
 import * as Yup from 'yup';
-import { TextField, Button, Box } from '@mui/material';
+import { TextField, Button, Box, Typography } from '@mui/material';
+import { Link } from 'react-router-dom';
 
 const CustomTextField = ({ label, ...props }) => (
   <Field
@@ -17,46 +18,51 @@ const CustomTextField = ({ label, ...props }) => (
   />
 );
 
-const LoginForm = ({ onSubmit }) => {
-  return (
-    <Formik
-      initialValues={{ username: '', password: '' }}
-      validationSchema={Yup.object({
-        username: Yup.string().required('Username is required'),
-        password: Yup.string().required('Password is required'),
-      })}
-      onSubmit={onSubmit}
-    >
-      {({ handleSubmit, resetForm, errors, touched }) => (
-        <Form onSubmit={handleSubmit}>
-          <Box>
-            <CustomTextField
-              name="username"
-              label="Username"
-              type="text"
-              helperText={<ErrorMessage name="username" />}
-              error={Boolean(touched.username && errors.username)}
-            />
-            <CustomTextField
-              name="password"
-              label="Password"
-              type="password"
-              helperText={<ErrorMessage name="password" />}
-              error={Boolean(touched.password && errors.password)}
-            />
-            <Button
-              type="submit"
-              variant="contained"
-              color="primary"
-              sx={{ mt: 2, width: '100%' }}
-            >
-              Login
+const LoginForm = ({ onSubmit }) => (
+  <Formik
+    initialValues={{ username: '', password: '' }}
+    validationSchema={Yup.object({
+      username: Yup.string().required('Username is required'),
+      password: Yup.string().required('Password is required'),
+    })}
+    onSubmit={onSubmit}
+  >
+    {({ handleSubmit, errors, touched, isSubmitting }) => (
+      <Form onSubmit={handleSubmit}>
+        <Box sx={{ maxWidth: 600, margin: 'auto', padding: 2 }}>
+          <CustomTextField
+            name="username"
+            label="Username"
+            type="text"
+            helperText={<ErrorMessage name="username" />}
+            error={Boolean(touched.username && errors.username)}
+          />
+          <CustomTextField
+            name="password"
+            label="Password"
+            type="password"
+            helperText={<ErrorMessage name="password" />}
+            error={Boolean(touched.password && errors.password)}
+          />
+          <Button
+            type="submit"
+            variant="contained"
+            color="primary"
+            sx={{ mt: 2, width: '100%' }}
+            disabled={isSubmitting}
+          >
+            Login
+          </Button>
+          <Typography variant="body2" align="center" sx={{ mt: 2 }}>
+            Don't have an account yet?{' '}
+            <Button component={Link} to="/signup" variant="text">
+              Sign Up
             </Button>
-          </Box>
-        </Form>
-      )}
-    </Formik>
-  );
-};
+          </Typography>
+        </Box>
+      </Form>
+    )}
+  </Formik>
+);
 
 export default LoginForm;
